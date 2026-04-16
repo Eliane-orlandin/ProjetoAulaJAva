ab69f800994542611fb07eda9b401758c3f9f21a
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-16 17:00:15 -0300

    Inicializando meu projeto

---
+++ src/introducao/HelloWord.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public class HelloWord {
+
+	public static void main(String[] args) {	// método principal que executa o código
+		
+		System.out.print("Hello World");
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,24 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		// Variáveis
+		int idade = 44;
+		String nome = "Eliane";
+		float graus = 20.0f;
+		boolean resposta;
+		
+		
+		// Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1414;
+		
+		System.out.println(nome);
+		System.out.println(QUANTIDADE);
+		System.out.println(graus);
+	}
+
+}
